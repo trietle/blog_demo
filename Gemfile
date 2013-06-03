452023d11cd52bf7a72adbb1f5896f8f01e5feb6
Triet Le <trietminhle09@gmail.com>
2013-06-03 14:50:38 +0700

    add vagrant

--- Gemfile
+++ Gemfile
@@ -15,11 +15,11 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  gem 'therubyracer', :platforms => :ruby
+  #gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
-
+gem 'puppet'
 gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
---
+++ puppet/manifests/precise32.pp
@@ -0,0 +1,33 @@
+# development.pp
+stage { 'req-install': before => Stage['rvm-install'] }
+
+class requirements {
+  group { "puppet": ensure => "present", }
+  exec { "apt-update":
+    command => "/usr/bin/apt-get -y update"
+  }
+
+  package {
+    ["mysql-client", "mysql-server", "libmysqlclient-dev"]:
+      ensure => installed, require => Exec['apt-update']
+  }
+}
+
+class installrvm {
+  include rvm
+  rvm::system_user { vagrant: ; }
+
+  if $rvm_installed == "true" {
+    rvm_system_ruby {
+      'ruby-2.0.0-p195':
+        ensure => 'present';
+    }
+  }
+}
+
+class doinstall {
+  class { requirements:, stage => "req-install" }
+  include installrvm
+}
+
+include doinstall
